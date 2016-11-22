8be2b1bdd9641805368996a6788762b40c71c991
josemam <jmlosvillares@correo.ugr.es>
2016-11-22 10:38:58 +0100

    Remodela ServidorChat
    Se ha separado main en varios métodos.

--- Prácticas/Ejercicio 5/ServidorChat.java
+++ Prácticas/Ejercicio 5/ServidorChat.java
@@ -3,27 +3,40 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServidorChat {
+  private static int port = 8989; // Puerto por defecto, puede cambiarse en los parámetros
+  private static ServerSocket socketServidor;
+
+  private static boolean initializeServerSocket() {
+    try {
+      socketServidor = new ServerSocket(port);
+    } catch(IOException e) {
+			System.err.println("Error al escuchar en el puerto " + port);
+      return false;
+    }
+    return true;
+  }
+
+  private static boolean keepListening() {
+    while (true) {
+      Socket socketServicio = null;
+      try {
+        socketServicio = socketServidor.accept();
+      } catch(IOException e){
+        System.out.println("Error: no se pudo aceptar la conexión solicitada");
+      }
+
+      ProcesadorChat procesador=new ProcesadorChat(socketServicio);
+      procesador.start();
+    }
+  }
+  
 	public static void main(String[] args) {
-    int port = 8989;
-    if (args.length == 1)
+    if (args.length >= 1)
       port = Integer.parseInt(args[0]);
 
-		try {
-			 ServerSocket socketServidor = new ServerSocket(port);
+    System.out.println("Esperando conexiones a través del puerto " + port + "...");
 
-			while (true) {
-				Socket socketServicio = null;
-				try {
-  				socketServicio = socketServidor.accept();
-				} catch(IOException e){
-				  System.out.println("Error: no se pudo aceptar la conexión solicitada");
-				}
-
-				ProcesadorChat procesador=new ProcesadorChat(socketServicio);
-				procesador.start();
-			}
-		} catch (IOException e) {
-			System.err.println("Error al escuchar en el puerto " + port);
-		}
+    if (initializeServerSocket())
+      keepListening();
 	}
 }
